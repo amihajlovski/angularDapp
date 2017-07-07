c343d805bbd1853f74fceb44eef627fd21a76969
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2017-07-07 13:51:18 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import web3 from 'web3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app';
+  web3 = web3;
+
+  initializeWeb3() {
+    if (typeof web3 !== 'undefined') {
+      this.web3 = new web3(web3.currentProvider);
+    } else {
+      // set the provider you want from Web3.providers
+      this.web3 = new web3(
+        new web3.providers.HttpProvider('http://localhost:8545'),
+      );
+    }
+  }
+
+  constructor() {
+    this.initializeWeb3();
+  }
 }
