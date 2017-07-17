20ae7f1412db23c7a1f12058ab722cd42d1d9349
Aleksandar Mihajlovski <aleksandar.mihajlovski@polarcape.com>
2017-07-17 09:54:17 +0200

    change blockchain network

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,4 +2,4 @@
  * Created by amihajlovski on 17.7.2017.
  */
 
-export const BLOCKCHAIN_NETWORK_URL = 'https://mainnet.infura.io/Iifbnul99MhPzVxwlBAn';
+export const BLOCKCHAIN_NETWORK_URL = 'https://ropsten.infura.io/Iifbnul99MhPzVxwlBAn';
